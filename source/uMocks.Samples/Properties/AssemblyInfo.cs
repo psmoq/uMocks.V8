cfb699f110fbeb3d6b79c2f2168667399b8be089
Przemysław Smokowski <przemyslaw.smokowski@rb.com>
2020-11-18 07:58:35 +0100

    Increased version number

--- source/uMocks.Samples/Properties/AssemblyInfo.cs
+++ source/uMocks.Samples/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("64af501e-1bb2-4160-8c6e-ea9cd039f2a2")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
